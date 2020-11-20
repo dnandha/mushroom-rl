30d9afd8a87c9eaee4106a9ebef4977ddf378fda
Cedric Derstroff <cedric.derstroff@gmx.net>
2020-11-20 20:08:26 +0100

    deleted RecurrentApproximator; everything works with the TorchApproximator now

--- mushroom_rl/approximators/parametric/__init__.py
+++ mushroom_rl/approximators/parametric/__init__.py
@@ -1,5 +1,4 @@
 from .linear import LinearApproximator
 from .torch_approximator import TorchApproximator
-from .recurrent_torch_approximator import RecurrentApproximator
 
-__all__ = ['LinearApproximator', 'TorchApproximator', 'RecurrentApproximator']
+__all__ = ['LinearApproximator', 'TorchApproximator']
