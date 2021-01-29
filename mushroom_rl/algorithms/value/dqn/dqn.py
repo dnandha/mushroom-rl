3fd01cf97e248f53b0e6cfd6094bc8946bbf1381
Cedric Derstroff <cedric.derstroff@gmx.net>
2021-01-29 13:36:32 +0100

    Revert "reset dqn to original"
    This reverts commit f9bee603

--- mushroom_rl/algorithms/value/dqn/dqn.py
+++ mushroom_rl/algorithms/value/dqn/dqn.py
@@ -175,6 +175,8 @@ class DQN(AbstractDQN):
     def _next_q(self, next_state, absorbing):
         q = self.target_approximator.predict(next_state)
         if np.any(absorbing):
-            q *= 1 - absorbing.reshape(-1, 1)
+            shape = list(q.shape)
+            shape[-1] = 1
+            q *= 1 - absorbing.reshape(shape)
 
-        return np.max(q, axis=1)
+        return np.max(q, axis=-1)
